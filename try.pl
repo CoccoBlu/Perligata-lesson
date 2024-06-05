1ba84d685f95a9fe411bc83f1c0bb138d84619e9
CoccoBlu <145787455+CoccoBlu@users.noreply.github.com>
2024-06-05 10:35:20 +0200

    Add files via upload

---
+++ try.pl
@@ -0,0 +1,42 @@
+use Lingua::Romana::Perligata;
+
+listis I tum III conscribementa da.
+
+per quisque in listis fac
+
+	sic
+
+		huic vestibulo perlegementum da.
+		
+		huic morde.
+		
+		dictum sic bel nome! cis tum novumversum egresso scribe.
+	
+	cis.
+
+io I da.
+
+novumversum egresso scribe.
+
+per quisque in listis fac
+	
+	sic
+	
+		ium tum lacunam tum hoc tum novumversum egresso scribe.
+		
+		io ium tum I addementum da.
+		
+	cis.
+
+novumversum egresso scribe.
+
+per quisque in listis fac
+	
+	sic
+	
+		hoc tum lacunam egresso scribe.
+		
+		
+	cis.
+
+dictum sic questi tre sono amici cis tum novumversum egresso scribe.
